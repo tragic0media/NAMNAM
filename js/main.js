6bf20b918821b6d87bc46a5ba47161c1ba1d8b30
Luna <lunngleez@gmail.com>
2026-06-27 21:36:39 +0200

    Imagenes y navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 // ÑAMÑAM - JavaScript principal
+
+// Menu movil
+var btnAbrir = document.getElementById("btnAbrirMenu");
+var btnCerrar = document.getElementById("btnCerrarMenu");
+var menuMovil = document.getElementById("menuMovil");
+
+if (btnAbrir) {
+    btnAbrir.addEventListener("click", function () {
+        menuMovil.classList.add("activo");
+    });
+}
+
+if (btnCerrar) {
+    btnCerrar.addEventListener("click", function () {
+        menuMovil.classList.remove("activo");
+    });
+}
